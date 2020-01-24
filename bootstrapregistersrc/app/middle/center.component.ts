d01b89596f7808b66fdb9804c6b7be8335b612bd
Anvesh-Babu <59701024+Anvesh-Babu@users.noreply.github.com>
2020-01-24 17:44:27 +0530

    Add files via upload

---
+++ bootstrapregistersrc/app/middle/center.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'middle',
+  templateUrl: './center.component.html',
+  styleUrls: ['./center.component.css']
+})
+export class CenterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
