d272740059d045c3179c0edee84d837ea4e33573
Anvesh-Babu <59701024+Anvesh-Babu@users.noreply.github.com>
2020-01-25 17:41:37 +0530

    Add files via upload

---
+++ src1/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MediaComponent } from './media/media.component';
+import { MedialistComponent } from './medialist/medialist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MediaComponent,
+  
+    MedialistComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
